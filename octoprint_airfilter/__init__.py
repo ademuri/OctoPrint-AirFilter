5b70ce0743cb325c3a0ddd0fd5cd6c5242a62344
Adam Demuri <adam.demuri@gmail.com>
2021-09-29 21:13:33 -0600

    Add settings

--- octoprint_airfilter/__init__.py
+++ octoprint_airfilter/__init__.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
+#import RPi.GPIO as GPIO
 
 ### (Don't forget to remove me)
 # This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
@@ -10,6 +11,7 @@ from __future__ import absolute_import
 # Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
+#import RPi.GPIO as GPIO
 
 class AirfilterPlugin(octoprint.plugin.SettingsPlugin,
     octoprint.plugin.AssetPlugin,
@@ -20,8 +22,21 @@ class AirfilterPlugin(octoprint.plugin.SettingsPlugin,
 
     def get_settings_defaults(self):
         return {
-            # put your plugin's default settings here
+            'pin': None,
+            'invert': False,
+            'is_pwm': True,
+            'pwm_frequency': 5000,
+            'pwm_duty_cycle': 90,
+            'enable_temperature_threshold': False,
+            'temperature_threshold': 60,
+            'print_start_trigger': True,
+            'print_end_delay': 600,
         }
+
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=False),
+        ]
 
     ##~~ AssetPlugin mixin
 
@@ -42,7 +57,7 @@ class AirfilterPlugin(octoprint.plugin.SettingsPlugin,
         # for details.
         return {
             "airfilter": {
-                "displayName": "Airfilter Plugin",
+                "displayName": "Air Filter",
                 "displayVersion": self._plugin_version,
 
                 # version check: github repository
@@ -60,14 +75,14 @@ class AirfilterPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Airfilter Plugin"
+__plugin_name__ = "Air Filter"
 
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
 #__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
 #__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
     global __plugin_implementation__
